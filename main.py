31cb4a0113e42b7d28a2dd36c62b39bb1a884772
chas751 <ch4s751@yandex.ru>
2025-10-14 18:54:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import telebot
 from flask import Flask, request
-import os
-
-TOKEN = TOKEN = "8357091966:AAGdvUQnHejEjJ9B5RG69tNad9bVt7G-_1M"
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
+
+import os
+TOKEN = TOKEN = "8357091966:AAGdvUQnHejEjJ9B5RG69tNad9bVt7G-_1M"
 @app.route('/')
 def home():
     return "Bot is alive!"
@@ -15,8 +15,3 @@ def home():
 def webhook():
     update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
     bot.process_new_updates([update])
-    return "OK", 200
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! 🌿 Я философский бот. Каждый день — новая мудрость!")
