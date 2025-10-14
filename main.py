80c0d29b57d801412058ba57440277ddb43960fc
chas751 <ch4s751@yandex.ru>
2025-10-14 19:45:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import telebot
 from flask import Flask, request
 
+# 🔑 ВСТАВЛЯЕМ ТОКЕН СЮДА:
+TOKEN = "8357091966:AAGdvUQnHejEjJ9B5RG69tNad9bVt7G-_1M"
+
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-
-import os
-TOKEN = TOKEN = "8357091966:AAGdvUQnHejEjJ9B5RG69tNad9bVt7G-_1M"
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "Bot is alive!", 200
 
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
-    update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
+    json_str = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
+    return 'OK', 200
+
+bot.remove_webhook()
+bot.set_webhook(url=f'https://philosoph-quote-bot.onrender.com/{TOKEN}')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет, Стас! Я живой бот ✌️")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
