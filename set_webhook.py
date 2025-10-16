757acbd5b86e4122dc56dd67a6f9a21ae9d2d3e9
chas751 <ch4s751@yandex.ru>
2025-10-16 15:48:27 +0300

    Delete set_webhook.py

--- set_webhook.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-TOKEN = "8228885470:AAFxS7h1Y5bYxSyjhAVG7FIahdSaJCoESBs"
-WEBHOOK_URL = "https://philosoph-quote-bot.onrender.com"
-
-set_webhook = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}"
-response = requests.get(set_webhook)
-print(response.json())
